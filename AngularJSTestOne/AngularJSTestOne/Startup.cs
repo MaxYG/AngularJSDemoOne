366b1931fa70c9f2e2eb61ca244ceac35bec51b9
TomYangGang <yangg@shinetechchina.com>
2014-06-25 17:48:53 +0800

    test

---
+++ AngularJSTestOne/AngularJSTestOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJSTestOne.Startup))]
+namespace AngularJSTestOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
