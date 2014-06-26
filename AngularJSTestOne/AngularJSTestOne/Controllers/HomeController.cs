3fcc52901b6ddffecdbc57b03e7ecccd7d344396
TomYangGang <yangg@shinetechchina.com>
2014-06-26 09:24:31 +0800

    test

--- AngularJSTestOne/AngularJSTestOne/Controllers/HomeController.cs
+++ AngularJSTestOne/AngularJSTestOne/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Anjular.Service;
+using AnjularJS.ViewModel;
 
 namespace AngularJSTestOne.Controllers
 {
     public class HomeController : Controller
     {
+         private readonly IEmployeeService _employeeService;
+
+         public HomeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var indexModel = new IndexViewModel
+            {
+                Employees = _employeeService.QueryAllStaffs()
+            };
+            return View("Index", indexModel);
+
         }
 
     }
--- AngularJSTestOne/AngularJSTestOne/Global.asax.cs
+++ AngularJSTestOne/AngularJSTestOne/Global.asax.cs
@@ -5,17 +5,46 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AngularJS.Data;
+using Anjular.Service;
+using AnjularJS.Repository;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
 
 namespace AngularJSTestOne
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer _container;
         protected void Application_Start()
         {
+            
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            _container = new WindsorContainer();
+            _container.Install(FromAssembly.This());
+
+            _container.Register(Classes.FromThisAssembly().BasedOn<IController>().LifestyleTransient());
+            _container.Register(Component.For<IDbAccess>().ImplementedBy<DbAccess>().LifestylePerWebRequest());
+            _container.Register(Component.For<IEmployeeRepository>().ImplementedBy<EmployeeRepository>().LifestylePerWebRequest());
+            _container.Register(Component.For<IEmployeeService>().ImplementedBy<EmployeeService>().LifestylePerWebRequest());
+            _container.Register(Component.For<AngularDb>().LifestyleSingleton());
+
+            var controllerFactory = new WindsorControllerFactory(_container);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+        protected void Application_End()
+        {
+            if (_container != null)
+            {
+                _container.Dispose();
+            }
+
         }
     }
 }
